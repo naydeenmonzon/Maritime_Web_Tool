5a20584dfce58d057951c90e2d100b8c8a1548f7
naydeenmonzon <naydeen.monzon@icloud.com>
2021-10-07 03:03:28 -0700

    Update settings.py

--- maritimeTool/settings.py
+++ maritimeTool/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
