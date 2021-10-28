1028550026858f5ce32c6e82f9876b95cb13bd49
naydeenmonzon <naydeen.monzon@icloud.com>
2021-10-28 14:39:29 -0700

    Update main.py

--- scrape/main.py
+++ scrape/main.py
@@ -48,7 +48,7 @@ def _init_browser():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--nosandbox")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     # driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
